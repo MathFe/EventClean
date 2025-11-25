adbcf3ce490a33c3da3825613b213aceebb60b83
Matheus <matheusferreirasjc@gmail.com>
2025-11-24 21:54:54 -0300

    feat: Injeta a dependencia dentro do EventoRepositoryGateway para utilizar o JPA, implementa o metodo criarEvento e mapeia o EventoEntity

--- src/main/java/dev/java10x/EventClean/infrastructure/gateway/EventoRepositoryGateway.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/gateway/EventoRepositoryGateway.java
@@ -1,6 +1,22 @@
 package dev.java10x.EventClean.infrastructure.gateway;
 
+import dev.java10x.EventClean.core.entities.Evento;
 import dev.java10x.EventClean.core.gateway.EventoGateway;
+import dev.java10x.EventClean.infrastructure.persistence.EventoRepository;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class EventoRepositoryGateway implements EventoGateway {
+
+    private final EventoRepository eventoRepository;
+
+    public EventoRepositoryGateway(EventoRepository eventoRepository) {
+        this.eventoRepository = eventoRepository;
+    }
+
+    @Override
+    public Evento criarEvento(Evento evento) {
+        return eventoRepository.save(evento);
+    }
 }
