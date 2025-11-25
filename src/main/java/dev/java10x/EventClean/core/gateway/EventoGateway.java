a6df7a41523e66d92039aeaec188a95ff82a1e6b
Matheus <matheusferreirasjc@gmail.com>
2025-11-24 21:51:14 -0300

    doc: Adiciona comentario

--- src/main/java/dev/java10x/EventClean/core/gateway/EventoGateway.java
+++ src/main/java/dev/java10x/EventClean/core/gateway/EventoGateway.java
@@ -1,4 +1,10 @@
 package dev.java10x.EventClean.core.gateway;
 
+import dev.java10x.EventClean.core.entities.Evento;
+
 public interface EventoGateway {
+
+
+    // Contrato
+    Evento criarEvento(Evento evento);
 }
