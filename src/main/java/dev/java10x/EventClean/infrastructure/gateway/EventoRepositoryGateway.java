d49ee7fd58efb39a4749c45b11bb7fa489aaafc7
Matheus <matheusferreirasjc@gmail.com>
2025-11-25 23:17:03 -0300

    feat: Injeta a dependencia do EntityMapper, adapta o nome do mapper nos padroes do clean code

--- src/main/java/dev/java10x/EventClean/infrastructure/gateway/EventoRepositoryGateway.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/gateway/EventoRepositoryGateway.java
@@ -2,6 +2,8 @@ package dev.java10x.EventClean.infrastructure.gateway;
 
 import dev.java10x.EventClean.core.entities.Evento;
 import dev.java10x.EventClean.core.gateway.EventoGateway;
+import dev.java10x.EventClean.infrastructure.mapper.EventoEntityMapper;
+import dev.java10x.EventClean.infrastructure.persistence.EventoEntity;
 import dev.java10x.EventClean.infrastructure.persistence.EventoRepository;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +12,17 @@ import org.springframework.stereotype.Component;
 public class EventoRepositoryGateway implements EventoGateway {
 
     private final EventoRepository eventoRepository;
+    private final EventoEntityMapper mapper;
 
-    public EventoRepositoryGateway(EventoRepository eventoRepository) {
+    public EventoRepositoryGateway(EventoRepository eventoRepository, EventoEntityMapper mapper) {
         this.eventoRepository = eventoRepository;
+        this.mapper = mapper;
     }
 
     @Override
     public Evento criarEvento(Evento evento) {
-        return eventoRepository.save(evento);
+        EventoEntity entity = mapper.toEntity(evento);
+        EventoEntity novoEvento = eventoRepository.save(entity);
+        return mapper.toDomain(novoEvento);
     }
 }
