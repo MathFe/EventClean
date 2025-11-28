cd95d5214c2949fb3886d1dd5ce2c4714c2f5ad9
Matheus <matheusferreirasjc@gmail.com>
2025-11-28 18:30:13 -0300

    feat: ajusta controller, incluindo stream e retorno de lista de DTO, finalizando o fluxo.

--- src/main/java/dev/java10x/EventClean/infrastructure/presentation/EventoController.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/presentation/EventoController.java
@@ -1,10 +1,14 @@
 package dev.java10x.EventClean.infrastructure.presentation;
 
 import dev.java10x.EventClean.core.entities.Evento;
+import dev.java10x.EventClean.core.usecases.BuscarEventoCase;
 import dev.java10x.EventClean.core.usecases.CriarEventoCase;
 import dev.java10x.EventClean.infrastructure.dtos.EventoDto;
 import dev.java10x.EventClean.infrastructure.mapper.EventoDtoMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -13,10 +17,13 @@ public class EventoController {
 
     private final CriarEventoCase criarEventoCase;
     private final EventoDtoMapper eventoDtoMapper;
+    private final BuscarEventoCase buscarEventoCase;
 
-    public EventoController(CriarEventoCase criarEventoCase, EventoDtoMapper eventoDtoMapper) {
+
+    public EventoController(CriarEventoCase criarEventoCase, EventoDtoMapper eventoDtoMapper, BuscarEventoCase buscarEventoCase) {
         this.criarEventoCase = criarEventoCase;
         this.eventoDtoMapper = eventoDtoMapper;
+        this.buscarEventoCase = buscarEventoCase;
     }
 
     @PostMapping("criarevento")
@@ -26,8 +33,13 @@ public class EventoController {
         return eventoDtoMapper.toDto(novoEvento);
     }
 
-    @GetMapping
-    public String listaEvento(){
-        return "lista de eventos";
+    @GetMapping("buscareventos")
+    public List<EventoDto> listarEvento() {
+        List<Evento> eventos = buscarEventoCase.execute();
+        List<EventoDto> eventosDto = eventos.stream()
+                .map(eventoDtoMapper::toDto)
+                .collect(Collectors.toList());
+        return eventosDto;
     }
+
 }
