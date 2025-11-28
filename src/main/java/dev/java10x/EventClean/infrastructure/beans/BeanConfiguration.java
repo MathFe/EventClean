79619276954f22e69904918ae6a253ae600bae64
Matheus <matheusferreirasjc@gmail.com>
2025-11-28 18:28:55 -0300

    feat: Implementa o bean para o metodo buscar eventos

--- src/main/java/dev/java10x/EventClean/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/beans/BeanConfiguration.java
@@ -1,6 +1,8 @@
 package dev.java10x.EventClean.infrastructure.beans;
 
 import dev.java10x.EventClean.core.gateway.EventoGateway;
+import dev.java10x.EventClean.core.usecases.BuscarEventoCase;
+import dev.java10x.EventClean.core.usecases.BuscarEventoCaseImpl;
 import dev.java10x.EventClean.core.usecases.CriarEventoCase;
 import dev.java10x.EventClean.core.usecases.CriarEventoCaseImpl;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +14,10 @@ public class BeanConfiguration {
     @Bean
     public CriarEventoCase criarEventoCase(EventoGateway eventoGateway) {
         return new CriarEventoCaseImpl(eventoGateway);
+    }
+
+    @Bean
+    public BuscarEventoCase buscarEventoCase(EventoGateway eventoGateway) {
+        return new BuscarEventoCaseImpl(eventoGateway);
     }
 }
